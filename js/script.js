5672a613a52c8593acb6e399ed7874f6238da9f1
hemant-7802 <hsharma9667@gmail.com>
2023-10-04 23:57:51 +0530

    JavaScript Added

---
+++ js/script.js
@@ -0,0 +1,51 @@
+var image=document.querySelector(".creation .image2 img")
+var span=document.querySelectorAll(".creation .image2 p span")
+span[0].setAttribute("class","headding")
+span[1].setAttribute("class","para")
+function img1()
+{
+    image.setAttribute("src","images/showcase-Poustka-1.png")
+    span[0].innerText="POUSTKA"
+    span[1].innerText=" BY MICHAL POUSTKA"
+}
+function img2()
+{
+    image.setAttribute("src","images/Mockup-Tabs-2-1.png")
+    span[0].innerText="BRANDPUNT MEDIA"
+    span[1].innerText=" BY PIETER GOORDEN"
+}
+function img3()
+{
+    image.setAttribute("src","images/Mockup-Tabs-1.png")
+    span[0].innerText="CHI KITCHEN ";
+    span[1].innerText=" BY TRAILBLAZE MARKETING"
+}
+function img4()
+{
+    image.setAttribute("src","images/Group-4185-1-2.png")
+    span[0].innerText="ON DUSK MARKETS"
+    span[1].innerText=" BY THE DESIGN SPACE"
+}
+function img5()
+{
+    image.setAttribute("src","images/Group-1087146.png")
+    span[0].innerText="ACCESS PARTNERS"
+    span[1].innerText=" BY DESIGN AND DEVELOPMENT MIND"
+}
+function img6()
+{
+    image.setAttribute("src","images/Group-1087521.png")
+    span[0].innerText="THE PERFECT LOAF"
+    span[1].innerText=" BY MAURIZIO LEO"
+}
+function img7()
+{
+    image.setAttribute("src","images/Group-1087521-1.png")
+    span[0].innerText="THE LITTLE PSYCHOLOGY CO."
+    span[1].innerText=" BY THE OBSERVATORY"
+}
+function main()
+{
+    image.setAttribute("src","images/Group-1087165.png")
+    span.innerText="TRELEMENTS"
+}
